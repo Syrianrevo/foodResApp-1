858c96dce96febf4dfedde9bd430c7476664ca24
Ammar Mohrat <ammar-mohrat@hotmail.com>
2019-07-23 11:26:43 -0400

    Initial commit

---
+++ src/main/java/com/syrianrevo/foodApp/FoodResApp1Application.java
@@ -0,0 +1,13 @@
+package com.syrianrevo.foodApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodResApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodResApp1Application.class, args);
+	}
+
+}
