e9f0369d725be8bc88a8d44133101f1b18b1c9f0
Mansoor <msmansoor@gmail.com>
2019-08-10 14:19:09 -0400

    Update import-h2.sql

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -5,5 +5,7 @@ REPLACE INTO `role` VALUES (2,'USER');
 
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, name, last_name)
+VALUES (1, '$2a$10$lQ5Pdyu9oYMjaNpywd3ZQunJlNgphzqyt4shlji24Y8cmaaCC0fu.', 'msmansoor@gmail.com', 'Mansoor', 'MS');
+INSERT INTO USER (user_id, password, email, name, last_name)
 VALUES (1, '$2a$10$lQ5Pdyu9oYMjaNpywd3ZQunJlNgphzqyt4shlji24Y8cmaaCC0fu.', 'ammar@hotmail.com', 'Ammar', 'Mohrat');
 
